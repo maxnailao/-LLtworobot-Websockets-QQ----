b5edd442fd0a271f5648118e294479bd54e07681
Ticheese <1849761280@qq.com>
2025-10-11 21:07:06 +0800

    Add files via upload

--- laoansuan.py
+++ laoansuan.py
@@ -1,4 +1,4 @@
-# 基于LLonerobot框架并用Websockets正向代理的QQ机器人--酪氨酸
+# 基于LLtworobot框架并用Websockets正向代理的QQ机器人--酪氨酸
 
 # 调用库
 import uuid
@@ -11,13 +11,13 @@ from datetime import datetime
 
 
 # 一般配置项
-uri = ""  # 替换为你的 LLOneBot WebSocket 正向地址
-qq = ''  # 机器人的qq账号
-group_id = 114514# 机器人工作的群号
+uri = "ws://localhost:3001"  # 替换为你的 LLOneBot WebSocket 正向地址
+qq = " "  # 机器人的qq账号
+group_id = " " #群号
 my_api_key = " "  # ai回复的API_KEY
-my_base_url = "h"  # AI代理地址
+my_base_url = " "  # 代理地址
 max_message = 15 #机器人消息记忆储存数额，越高token消耗越快
-robot_background = """ """#AI人设性格设置
+robot_background = """ """ #机器人设定
 
 #创建一些要用到的变量
 ai_messages = [
@@ -34,172 +34,183 @@ async def receive_messages(ws):  # 循环接收消息
         message_data = json.loads(message)# 将字符串解析为字典
         if message_data.get('message_type') == 'group':
             print(f"""
-----------------------"消息类型":{message_data.get('message_type', 'Unknown')}----------------------
+        ----------------------"消息类型":{message_data.get('message_type', 'Unknown')}----------------------
         \n“收到消息”: {message}
----------------------------------------------------------------------------------------------------       
-""")
+        ---------------------------------------------------------------------------------------------------       
+        """)
         elif message_data.get('post_type') == 'meta_event':
             print(f"""
-----------------------"消息类型":meta_event---------------------------------------------------------
+        ----------------------"消息类型":meta_event---------------------------------------------------------
         \n“收到消息”: {message}
----------------------------------------------------------------------------------------------------       
+        ---------------------------------------------------------------------------------------------------       
         """)
         else:
             print(f"""
-----------------------"消息类型":其他---------------------------------------------------------
+        ----------------------"消息类型":其他---------------------------------------------------------
         \n {message}
----------------------------------------------------------------------------------------------------       
+        ---------------------------------------------------------------------------------------------------       
         """)
         return message_data
 
 
 async def main():  # 循环发送消息
     async with websockets.connect(uri) as websocket:
-
+        echo_like = None
         while True:
             receive_data = await receive_messages(websocket)
             if receive_data.get('post_type') == 'meta_event' and receive_data.get('sub_type') == 'connect':
                 print('邦邦卡邦，酪氨酸启动完成，ο(=•ω＜=)ρ⌒☆')
             check_and_clear_dk_list()#检查并清除字典
-            #处理群消息
-            if receive_data.get('message_type') == 'group':
-                #处理群一般消息
-                if receive_data.get('post_type') == 'message':
-                    if 'message' in receive_data:
-                        messages = receive_data['message']
-                        if messages:
-                            for i in range(0, len(messages)):  # 遍历消息段
-                                if messages[i]['type'] == 'at' and messages[i]['data']['qq'] == qq:
+            #处理群一般消息
+            if receive_data.get('post_type') == 'message' and str(receive_data['group_id']) == group_id:
+                if 'message' in receive_data:
+                    messages = receive_data['message']
+                    if messages:
+                        for i in range(0, len(messages)):  # 遍历消息段
+                            if messages[i]['type'] == 'at' and messages[i]['data']['qq'] == qq:
 
 
-                                    if receive_data['message'][i-1]['data']['text'].startswith(' #luck'):#luck指令
-                                        if any(user['user'] == receive_data['sender']['nickname']  for user in dk_list):
-                                            length = get_user_length(receive_data['sender']['nickname'])
-                                            echo = str(uuid.uuid4())
-                                            data = {
-                                                "action": "send_group_msg",
-                                                "params": {
-                                                    "group_id": group_id,
-                                                    "message": f"你已经抽过啦，今日的牛牛长度为{length}cm"
-                                                },
-                                                'echo': echo
-                                            }
-                                            await websocket.send(json.dumps(data))
-                                        else:
-                                            dk_response_message = dk(receive_data['sender']['nickname'])
-                                            echo = str(uuid.uuid4())
-                                            data = {
-                                                "action": "send_group_msg",
-                                                "params": {
-                                                    "group_id": group_id,
-                                                    "message": dk_response_message
-                                                },
-                                                'echo': echo
-                                            }
-                                            await websocket.send(json.dumps(data))
-
-                                    elif receive_data['message'][i-1]['data']['text'].startswith(' #list_luck'):#list_luck指令
-                                        if dk_list:
-                                            #对dk_list内依据length大小进行排序
-                                            dk_list.sort(key=lambda x: x['length'], reverse=True)
-                                            # 格式化为字符串
-                                            dk_list_str = "\n".join(
-                                                [f" {item['user']}, {item['length']}cm" for item in dk_list])
-                                            echo = str(uuid.uuid4())
-                                            data = {
-                                                "action": "send_group_msg",
-                                                "params": {
-                                                    "group_id": group_id,
-                                                    "message": "今日牛牛排行榜如下:\n"+dk_list_str
-                                                },
-                                                'echo': echo
-                                            }
-                                            await websocket.send(json.dumps(data))
-
-                                        else:
-                                            echo = str(uuid.uuid4())
-                                            data = {
-                                                "action": "send_group_msg",
-                                                "params": {
-                                                    "group_id": group_id,
-                                                    "message": "今天还没有人抽取幸运数字哦"
-                                                },
-                                                'echo': echo
-                                            }
-                                            await websocket.send(json.dumps(data))
-
-                                    elif receive_data['message'][i-1]['data']['text'].startswith(' #help'):#help指令
+                                if receive_data['message'][i-1]['data']['text'].startswith(' #luck'):#luck指令
+                                    if any(user['user'] == receive_data['sender']['nickname']  for user in dk_list):
+                                        length = get_user_length(receive_data['sender']['nickname'])
                                         echo = str(uuid.uuid4())
                                         data = {
                                             "action": "send_group_msg",
                                             "params": {
                                                 "group_id": group_id,
-                                                "message": help_order()
+                                                "message": f"你已经抽过啦，今日的牛牛长度为{length}cm"
+                                            },
+                                            'echo': echo
+                                        }
+                                        await websocket.send(json.dumps(data))
+                                    else:
+                                        dk_response_message = dk(receive_data['sender']['nickname'])
+                                        echo = str(uuid.uuid4())
+                                        data = {
+                                            "action": "send_group_msg",
+                                            "params": {
+                                                "group_id": group_id,
+                                                "message": dk_response_message
+                                            },
+                                            'echo': echo
+                                        }
+                                        await websocket.send(json.dumps(data))
+
+                                elif receive_data['message'][i-1]['data']['text'].startswith(' #list_luck'):#list_luck指令
+                                    if dk_list:
+                                        #对dk_list内依据length大小进行排序
+                                        dk_list.sort(key=lambda x: x['length'], reverse=True)
+                                        # 格式化为字符串
+                                        dk_list_str = "\n".join(
+                                            [f" {item['user']}, {item['length']}cm" for item in dk_list])
+                                        echo = str(uuid.uuid4())
+                                        data = {
+                                            "action": "send_group_msg",
+                                            "params": {
+                                                "group_id": group_id,
+                                                "message": "今日牛牛排行榜如下:\n"+dk_list_str
+                                            },
+                                            'echo': echo
+                                        }
+                                        await websocket.send(json.dumps(data))
+
+                                    else:
+                                        echo = str(uuid.uuid4())
+                                        data = {
+                                            "action": "send_group_msg",
+                                            "params": {
+                                                "group_id": group_id,
+                                                "message": "今天还没有人抽取幸运数字哦"
+                                            },
+                                            'echo': echo
+                                        }
+                                        await websocket.send(json.dumps(data))
+
+                                elif receive_data['message'][i-1]['data']['text'].startswith(' #help'):#help指令
+                                    echo = str(uuid.uuid4())
+                                    data = {
+                                        "action": "send_group_msg",
+                                        "params": {
+                                            "group_id": group_id,
+                                            "message": help_order()
+                                        },
+                                        'echo': echo
+                                    }
+                                    await websocket.send(json.dumps(data))
+
+
+                                elif receive_data['message'][i-1]['data']['text'].startswith(' #like'):#like指令
+                                    try:
+                                        echo = str(uuid.uuid4())
+                                        data = {
+                                            "action": "send_group_msg",
+                                            "params": {
+                                                "group_id": group_id,
+                                                "message": "正在点赞中..."
+                                            },
+                                            'echo': echo
+                                        }
+                                        await websocket.send(json.dumps(data))
+
+                                        echo_like = str(uuid.uuid4())
+                                        data = {
+                                            "action": "send_like",
+                                            "params": {
+                                                "user_id": receive_data['user_id'],
+                                                "times": 10
+                                            },
+                                            'echo': echo_like
+                                        }
+                                        await websocket.send(json.dumps(data))
+                                        print('点赞成功')
+
+
+                                    except:
+                                        echo = str(uuid.uuid4())
+                                        data = {
+                                            "action": "send_group_msg",
+                                            "params": {
+                                                "group_id": group_id,
+                                                "message": "点赞失败喵qwq"
                                             },
                                             'echo': echo
                                         }
                                         await websocket.send(json.dumps(data))
 
 
-                                    elif receive_data['message'][i-1]['data']['text'].startswith(' #like'):#like指令
-                                        try:
-                                            echo = str(uuid.uuid4())
-                                            data = {
-                                                "action": "send_like",
-                                                "params": {
-                                                    "user_id": receive_data['user_id'],
-                                                    "times": 10
-                                                },
-                                                'echo': echo
-                                            }
-                                            await websocket.send(json.dumps(data))
+                                else:#调用AI回答
+                                    message_text = await ai_respond(receive_data['sender']['nickname'], receive_data['message'][i - 1]['data']['text'])
+                                    echo = str(uuid.uuid4())
+                                    data = {
+                                        "action": "send_group_msg",
+                                        "params": {
+                                            "group_id": group_id,
+                                            "message": message_text
+                                        },
+                                        'echo': echo
+                                    }
+                                    await websocket.send(json.dumps(data))
 
-                                            echo = str(uuid.uuid4())
-                                            data = {
-                                                "action": "send_group_msg",
-                                                "params": {
-                                                    "group_id": group_id,
-                                                    "message": "就让酪氨酸来给主人点10个攒吧喵"
-                                                },
-                                                'echo': echo
-                                            }
-                                            await websocket.send(json.dumps(data))
-
-
-                                        except:
-                                            echo = str(uuid.uuid4())
-                                            data = {
-                                                "action": "send_group_msg",
-                                                "params": {
-                                                    "group_id": group_id,
-                                                    "message": "点赞失败喵qwq"
-                                                },
-                                                'echo': echo
-                                            }
-                                            await websocket.send(json.dumps(data))
-
-
-                                    else:#调用AI回答
-                                        message_text = await ai_respond(receive_data['sender']['nickname'], receive_data['message'][i - 1]['data']['text'])
-                                        echo = str(uuid.uuid4())
-                                        data = {
-                                            "action": "send_group_msg",
-                                            "params": {
-                                                "group_id": group_id,
-                                                "message": message_text
-                                            },
-                                            'echo': echo
-                                        }
-                                        await websocket.send(json.dumps(data))
-
-                #处理点赞返回的消息
-                if receive_data.get('message') == '今日同一好友点赞数已达上限':
+            #处理点赞失败返回的消息
+            if echo_like is not None:
+                if receive_data.get('status') == 'failed' and receive_data.get('echo') == echo_like:
                     echo = str(uuid.uuid4())
                     data = {
                         "action": "send_group_msg",
                         "params": {
                             "group_id": group_id,
-                            "message": "不过今天已经给你点过赞了，不能再点了喵"
+                            "message": "今天已经给你点过赞了，不能再点了喵"
+                        },
+                        'echo': echo
+                    }
+                    await websocket.send(json.dumps(data))
+                if receive_data.get('status') == 'ok' and receive_data.get('echo') == echo_like:
+                    echo = str(uuid.uuid4())
+                    data = {
+                        "action": "send_group_msg",
+                        "params": {
+                            "group_id": group_id,
+                            "message": "点赞成功喵"
                         },
                         'echo': echo
                     }
@@ -248,8 +259,9 @@ def dk(sender):
     elif length > 20:
         return sender+f"的牛牛长度为{length}cm，WOW!"
 
-#调用以清除dk_list内部数据
+
 def check_and_clear_dk_list():
+    """检查并清除字典（在循环中调用）"""
     global dk_list, last_clear_date
 
     current_date = datetime.now().date()
@@ -273,5 +285,4 @@ def help_order():
              #list_luck:查询今日幸运榜单
              #like:给个人资料点赞喵"""
     return order
-
 asyncio.run(main())
